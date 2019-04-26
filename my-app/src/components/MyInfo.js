95f9d3271a693acdc49f2ed68e3fddb32c0dead9
Oscar Chamat <oscarchamat@useitweb.com>
2019-04-26 14:00:43 -0500

    move components into separate files

---
+++ my-app/src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo (){
+    return (
+        <div>
+            <h1>Óscar</h1>
+            <p>I am cute</p>
+            <ol>
+                <li>Paris</li>
+                <li>Machu pichu</li>
+                <li>La india</li>
+            </ol>
+        </div>
+    )
+}
+
+export default MyInfo
